a8a0b34505b8eb83307f922edb672490b634ef64
Antonia Males <antoniamales24@gmail.com>
2022-12-19 23:24:45 +0100

    Added query 1

--- CERNAtHome-Queries.sql
+++ CERNAtHome-Queries.sql
@@ -7,3 +7,44 @@ SELECT * FROM Cities
 SELECT * FROM Hotels
 SELECT * FROM Scientists
 SELECT * FROM PapersAuthors
+
+---Query 1 - Names, Publication dates and Authors for each Scientific paper---
+
+SELECT
+	sp.Name, sp.PublicationDate, 
+	(SELECT 
+	 	STRING_AGG(CONCAT(s.Surname,' ,',LEFT(s.Name,1),'.'),'; ') 
+	 FROM 
+	 	Scientists s
+	 WHERE
+	 	(SELECT 
+		 	COUNT(*) 
+		 FROM 
+		 	PapersAuthors pa
+		 WHERE
+			pa.AuthorId = s.ScientistId AND pa.PaperId = sp.PaperId) > 0)
+	AS Authors
+FROM
+	ScientificPapers sp
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
